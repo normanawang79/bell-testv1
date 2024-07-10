2df1ff4048e971318942aa22f86777f239b36013
normanawang79 <norman.awang.07@gmail.com>
2024-07-10 10:07:30 +0800

    ftyfuyfu

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-
+const prompt = require('prompt-sync')()
+let age = prompt("Please enter your age: ");
+age = parseInt(age)
+let olderage = age + 20;
+console.log('In 20 years time, you will be', olderage);
