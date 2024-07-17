b4cc20f7ff1a8c2983165d9fcef5fef663b125c3
normanawang79 <norman.awang.07@gmail.com>
2024-07-17 12:11:58 +0800

    made changes to todolist app

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')()
 let age = prompt("Please enter your age: ");
 age = parseInt(age)
-let olderage = age + 20;
-console.log('In 20 years time, you will be', olderage);
+let youngerage = age - 20;
+console.log('20 years ago, you were', youngerage);
